65d5470e79f3f4cf3e0472fb62981bc4c3146060
Yuli Levtov <yulilevtov@gmail.com>
2015-11-18 15:53:22 +0000

    Now creating track abstractions and some util stuff

--- Python/converter.py
+++ Python/converter.py
@@ -6,6 +6,7 @@ import xml.etree.ElementTree as ET
 
 # Array to store the samples present in the Live set
 samples = []
+tracks = []
 
 def main():
     parser = argparse.ArgumentParser(
@@ -33,15 +34,58 @@ def parseLiveXML(file):
     
     print "Samples: ", samples
 
+    # Find all audio tracks
+    for node in root.findall(".//Tracks/AudioTrack/Name/EffectiveName"):
+        trackName = node.attrib.get("Value")
+        # To do: remove whitespace from track name
+        tracks.append(trackName)
+
+    print "Tracks: ", tracks
+
 def generatePdPatch(path):
     file = open(path, 'w')
     # Write the initial canvas line
-    file.write("#N canvas 203 222 450 300 10;")
+    file.write("#N canvas 203 222 450 300 10;\n")
 
+    # INIT
+    # Add the declare objects for extra paths (inside init subpatch)
+    file.write("#X declare -path abs;\n")
+    file.write("#X declare -path audio;\n")
+    file.write("#N canvas 30 30 450 300 init 0;\n")
+    file.write("#X obj 30 30 declare -path abs;\n")
+    file.write("#X obj 30 50 declare -path audio;\n")
+    file.write("#X restore 30 30 pd init;\n")
+
+    # SAMPLES
+    # Subpatch for samplebank objects
+    file.write("#N canvas 30 50 450 300 samplebanks 0;\n")
+    # Starting y-pos for objects
+    yPos = 60
     # Create a samplebank object for each sample
     for sample in samples:
-        sampleObj = "#X obj 30 30 u_samplebank "+sample 
+        sampleObj = "#X obj 30 "+str(yPos)+" u_samplebank "+sample+";\n"
         file.write(sampleObj)
+        yPos = yPos+20
+    # Close subpatch
+    file.write("#X restore 30 50 pd samplebanks;\n")
+
+    # TRACKS
+    # Subpatch for tracks
+    file.write("#N canvas 30 70 450 300 tracks 0;\n")
+    # Starting y-pos for objects
+    yPos = 60
+    # Create a samplebank object for each sample
+    for track in tracks:
+        trackObj = "#X obj 30 "+str(yPos)+" u_track "+track+";\n"
+        file.write(trackObj)
+        yPos = yPos+20
+    # Close subpatch
+    file.write("#X restore 30 70 pd tracks;\n")
+
+    # Create timebase abstraction
+    file.write("#X obj 30 90 u_timebase;\n")
+    # Create output abstraction
+    file.write("#X obj 30 110 u_output;\n")
 
     file.close()
 
